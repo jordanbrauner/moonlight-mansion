437b4116bcbbf549a7181077c8a2a750b6e2e5cd
Jordan Brauner <jordan.brauner@gmail.com>
2016-01-10 12:09:19 -0500

    Restructured Schema

--- db/schema.js
+++ db/schema.js
@@ -8,12 +8,22 @@ var ObjectId = Schema.ObjectId;
 // Define Schema for cards
 var CardSchema = new Schema({
   cardType: String,
+  roomType: String,
   cardName: String,
   flavorText: String,
-  actions: [],
-  fortuneMod: Number,
-  sanityMod: Number,
-  moonMod: Number
+  useItem: {
+    itemFate: Number,
+    itemResult: []
+  },
+  actions: {
+    action1: {
+      actionFate: Number,
+      fortune: [],
+      hardship: []
+    },
+    action2: [],
+    action3: String
+  },
 });
 
 // Set models in Mongoose utilizing the Schema
