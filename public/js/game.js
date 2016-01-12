45fe3bf8e8ffa772887dac1a79df1dff78990035
Jordan Brauner <jordan.brauner@gmail.com>
2016-01-12 15:13:37 -0500

    Working on map

--- public/js/game.js
+++ public/js/game.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   // Set Variables
   eventDeck = [];
   itemDeck = [];
-  var eventCard = [];
-  var inventory = [];
-  var discarded = [];
+  eventCard = [];
+  inventory = [];
+  discarded = [];
 
   // Fetch JSON card data
   Card.fetch().then(function() {
@@ -29,7 +29,6 @@ $(document).ready(function() {
       game.shuffleDeck(eventDeck);
 
       // NOTE CODE BELOW FOR TESTING PURPOSE ONLY
-      game.drawEventCard(eventDeck);
       console.log("Game started");
 
       $("#shuffle").on("click", function() {
@@ -46,12 +45,14 @@ $(document).ready(function() {
     },
 
     /////////////////////////////////////////////////////////////////////////
-    shuffleDeck: function(toShuffle) {
+    shuffleDeck: function(toShuffle, discarded) {
     /////////////////////////////////////////////////////////////////////////
-      // Clear all-cards div
+      console.log("shuffling!");
       $(".all-cards").html("");
-      // Shuffle all cards from input randomly into item or event decks
-      // console.log("toShuffle's cardType: " + toShuffle[0].cardType);
+      for (var num in discarded) {
+        toShuffle.push(discarded[num]);
+        console.log("Pushing " + discarded[num].cardName + "to the toShuffle deck");
+      }
       var cardsLeft = toShuffle.length;
       var i;
       while (cardsLeft) {
@@ -70,26 +71,56 @@ $(document).ready(function() {
     /////////////////////////////////////////////////////////////////////////
     drawEventCard: function() {
     /////////////////////////////////////////////////////////////////////////
-      eventCard.push(eventDeck.splice(0, 1)[0]);
-      var drawnCard = eventCard[0];
-      $("#room-type").html(drawnCard.roomType);
-      $("#card-name").html(drawnCard.cardName);
-      $("#flavor-text").html(drawnCard.flavorText);
+      if (eventDeck.length > 0) {
+        eventCard.push(eventDeck.splice(0, 1)[0]);
+        var drawnCard = eventCard[0];
+        $("#card-wrapper #room-type").html(drawnCard.roomType);
+        $("#card-wrapper .card-name").html(drawnCard.cardName);
+        $("#card-wrapper .flavor-text").html(drawnCard.flavorText);
 
-      $("#a1-name").html(drawnCard.actions.action1.a1Name);
-      $("#a1-result").html(drawnCard.actions.action1.fortune);
+        $("#a1-name").html(drawnCard.actions.action1.a1Name);
+        $("#a1-fortune").html("<strong>Fortune: </strong>" + drawnCard.actions.action1.fortune);
+        $("#a1-hardship").html("<strong>Hardship: </strong>" + drawnCard.actions.action1.hardship);
+        $("#action-1").on("click", function() {
+          game.discardEventCard(eventCard);
+        });
 
-      $("#a2-name").html(drawnCard.actions.action2.a2Name);
-      $("#a2-result").html(drawnCard.actions.action2.a2Result);
+        $("#a2-name").html(drawnCard.actions.action2.a2Name);
+        $("#a2-result").html(drawnCard.actions.action2.a2Result);
 
-      $("#a3-name").html(drawnCard.actions.action3.a3Name);
-      $("#a3-result").html(drawnCard.actions.action3.a3Result);
+        $("#a3-name").html(drawnCard.actions.action3.a3Name);
+        $("#a3-result").html(drawnCard.actions.action3.a3Result);
+      } else {
+        game.shuffleDeck(eventDeck, discarded);
+        game.drawEventCard();
+      }
+    },
+
+    /////////////////////////////////////////////////////////////////////////
+    discardEventCard: function(card) {
+    /////////////////////////////////////////////////////////////////////////
+      discarded.push(card.splice(0, 1)[0]);
+      var justAdded = discarded.length - 1;
+      console.log("Card discarded: " + discarded[justAdded].cardName);
+      $("#card-wrapper #room-type").html("");
+      $("#card-wrapper .card-name").html("");
+      $("#card-wrapper .flavor-text").html("");
+
+      $("#a1-name").html("");
+      $("#a1-fortune").html("");
+      $("#a1-hardship").html("");
+
+      $("#a2-name").html("");
+      $("#a2-result").html("");
+
+      $("#a3-name").html("");
+      $("#a3-result").html("");
     },
 
     /////////////////////////////////////////////////////////////////////////
     drawItemCard: function() {
     /////////////////////////////////////////////////////////////////////////
-      if (inventory < 5) {
+      if (inventory.length < 5) {
         inventory.push(itemDeck.splice(0, 1)[0]);
         game.updateInventory();
       } else {
